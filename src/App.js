e4719fbca8e7be5b69a2cc87d44e8f857fa47286
ametzamaldonado <ametza.maldonado@gmail.com>
2023-05-10 20:45:20 -0400

    Added sample navbar and moved Router to index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Profile from "./components/Profile/Profile";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {/* --------- NavBar Component could go here --------- */}
+         <nav>  {/* <- simplified navbar, only meant as an example */}
+          <ul className="links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/profile">Profile</a></li>
+            <li><a href="#Other1">Other 1</a></li>
+            <li><a href="#Other2">Other 2</a></li>  
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
